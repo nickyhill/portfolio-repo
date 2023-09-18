dc5994e103287b970f70d5ddf0fc424a736b725e
Nicholas Hillengas <89429297+nickyhill@users.noreply.github.com>
2023-09-18 23:15:24 +0000

    Added education class and json file

---
+++ src/BlazorApp/Models/EducationMe.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp.Models;
+
+public class EducationMe
+{
+    public string School { get; set; } = string.Empty;
+    public string Grade { get; set; } = string.Empty;
+    public string ExpectedGrad { get; set; } = string.Empty;
+    public string Major { get; set; } = string.Empty;
+}
