bcd6c17d35ba3d1dbd84f4da3503dcd4bdd8438e
Nicholas Hillengas <89429297+nickyhill@users.noreply.github.com>
2023-09-20 03:28:56 +0000

    finished styling work experience and education

---
+++ src/BlazorApp/Models/workMy.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp.Models;
+
+public class WorkMe
+{
+    public string Company { get; set; } = string.Empty;
+    public string Position { get; set; } = string.Empty;
+    public string TimeFrame { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    
+}
--- src/BlazorApp/wwwroot/js/site.js
+++ src/BlazorApp/wwwroot/js/site.js
@@ -1,12 +1,25 @@
 function toggleEducation() {
     var educationContainer = document.querySelector('.education-container');
-    var arrowElement = document.getElementById('arrow');
+    var arrowElement = document.getElementById('arrowEdu');
 
     if (educationContainer.style.display === 'none' || educationContainer.style.display === '') {
         educationContainer.style.display = 'block';
         arrowElement.textContent = '▼'; // Change the arrow to pointing down
     } else {
         educationContainer.style.display = 'none';
+        arrowElement.textContent = '▶'; // Change the arrow to pointing right
+    }
+}
+
+function toggleWork() {
+    var workContainer = document.querySelector('.work-container');
+    var arrowElement = document.getElementById('arrowWork');
+
+    if (workContainer.style.display === 'none' || workContainer.style.display === '') {
+        workContainer.style.display = 'block';
+        arrowElement.textContent = '▼'; // Change the arrow to pointing down
+    } else {
+        workContainer.style.display = 'none';
         arrowElement.textContent = '▶'; // Change the arrow to pointing right
     }
 }
